56cf344b5566e4e48b0e21821c4915d6f61e7997
Máximo Ladner <165179887+MaximoLadner@users.noreply.github.com>
2024-10-20 18:13:33 -0300

    Creacion de menu y opciones 1 y 4.

---
+++ Malopo/TrabajoPracticoAgustin/models/DesarrolloWeb.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TrabajoPracticoAgustin.enums;
+
+namespace TrabajoPracticoAgustin.models
+{
+    public class DesarrolloWeb : Proyecto
+    {
+        public string TecnologiaAusar {  get; set; }
+        public DesarrolloWeb(string nombre, EstadoProyecto estado, int desarrolladores, string TecnologiaAusar)
+            : base(nombre, estado, desarrolladores)
+        {
+            this.TecnologiaAusar = TecnologiaAusar;
+        }
+    }
+}
